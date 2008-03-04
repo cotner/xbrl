f61717354816ca36aff4ccc0c9ac157ea84b41c0
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2008-03-04 13:24:24 +0000

    Moved async discovery helper classes and tests to the XBRLAPI project.

--- module-api/src/test/java/org/xbrlapi/loader/tests/AllTests.java
+++ module-api/src/test/java/org/xbrlapi/loader/tests/AllTests.java
@@ -8,6 +8,7 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for org.xbrlapi.loader.tests");
 		//$JUnit-BEGIN$
+        suite.addTestSuite(AsyncLoaderImplTestCase.class);
 		suite.addTestSuite(LoaderImplTestCase.class);
 		//$JUnit-END$
 		return suite;
