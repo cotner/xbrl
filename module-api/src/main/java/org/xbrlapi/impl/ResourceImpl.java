493ac2a396cda9770742a27ae79c746cbb822e6f
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2008-04-07 12:00:33 +0000

    Punctuation change in documentation.

--- module-api/src/main/java/org/xbrlapi/impl/ResourceImpl.java
+++ module-api/src/main/java/org/xbrlapi/impl/ResourceImpl.java
@@ -15,7 +15,7 @@ public class ResourceImpl extends ArcEndImpl implements Resource {
 	 * Get the value of the resource role (xlink:role attribute) for the resource.
 	 * @return the value of the resource role (xlink:role attribute) for the resource or 
 	 * null if none is specified.
-	 * @throws XBRLException.
+	 * @throws XBRLException
 	 * @see org.xbrlapi.Resource#getResourceRole()
 	 */
 	public String getResourceRole() throws XBRLException {
