c49fc5aefb919b448df48066384c591bfc02f81c
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2008-08-05 01:57:26 +0000

    Defined the XDT namespace and prefix constants.

---
+++ module-xdt/src/main/java/org/xbrlapi/xdt/XDTConstants.java
@@ -0,0 +1,15 @@
+package org.xbrlapi.xdt;
+
+/**
+ * Defines a range of constants (namespaces etc) that are used for the XDT module.
+ * @author Geoffrey Shuetrim (geoff@galexy.net)
+ */
+public class XDTConstants {
+
+    public final  static String XBRLDTNamespace = "http://xbrl.org/2005/xbrldt";
+    public final  static String XBRLDTPrefix = "xbrldt";
+
+    public final  static String XBRLDINamespace = "http://xbrl.org/2005/xbrldi";
+    public final  static String XBRLDIPrefix = "xbrldi";
+
+}
