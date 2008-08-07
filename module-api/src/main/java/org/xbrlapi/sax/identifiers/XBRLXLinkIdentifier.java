6bad2033539c68b5967b9575840ee0d392dc93df
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2008-08-07 06:34:34 +0000

    Name harmonised with rest of package.

---
+++ module-api/src/main/java/org/xbrlapi/sax/identifiers/XBRLXLinkIdentifier.java
@@ -0,0 +1,60 @@
+package org.xbrlapi.sax.identifiers;
+
+import org.xbrlapi.loader.Loader;
+import org.xbrlapi.sax.ContentHandler;
+import org.xbrlapi.utilities.XBRLException;
+import org.xbrlapi.xlink.XLinkException;
+import org.xbrlapi.xlink.XLinkHandler;
+import org.xbrlapi.xlink.XLinkProcessor;
+import org.xbrlapi.xlink.handler.XBRLXLinkHandlerImpl;
+import org.xml.sax.Attributes;
+
+/**
+ * @author Geoffrey Shuetrim (geoff@galexy.net)
+ */
+
+public class XBRLXLinkIdentifier extends BaseIdentifier implements Identifier {
+
+    /**
+     * @see org.xbrlapi.sax.identifiers.BaseIdentifier#BaseFragmentIdentifierImpl(Loader)
+     */
+    public XBRLXLinkIdentifier(ContentHandler contentHandler) throws XBRLException {
+        super(contentHandler);
+    }
+
+    private XLinkProcessor getXLinkProcessor() {
+        return getLoader().getXlinkProcessor();
+    }
+
+    private XLinkHandler getXLinkHandler() {
+        return getLoader().getXlinkProcessor().getXLinkHandler();
+    }
+
+    /**
+     * Passes responsibility along to the XLink handler via the XLink Processor.
+     * 
+     * @see org.xbrlapi.sax.identifiers.BaseIdentifier#startElement(String,String,String,Attributes)
+     */
+    public void startElement(
+            String namespaceURI, 
+            String lName, 
+            String qName,
+            Attributes attrs) throws XBRLException {
+
+        // Set the Element state information in the XBRL XLink handler.
+        try {
+            XBRLXLinkHandlerImpl xlinkHandler = (XBRLXLinkHandlerImpl) this.getXLinkHandler();
+            xlinkHandler.setElementState(this.getElementState());
+        } catch (ClassCastException e) {
+            throw new XBRLException("The XBRLXLinkIdentifier must be used with an XBRLXLinkHandler.",e);
+        }
+            
+        try {
+            getXLinkProcessor().startElement(namespaceURI,lName,qName,attrs);
+        } catch (XLinkException e) {
+            throw new XBRLException("XLink processing of the start of an element failed.",e);
+        }
+        
+    }
+    
+}
