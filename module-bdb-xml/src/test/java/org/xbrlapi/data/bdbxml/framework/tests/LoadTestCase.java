6442f67ddac3b8fd29c0b3f5b128cb1be2344ca3
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2008-10-15 00:13:50 +0000

    Updated the data base location.

--- module-bdb-xml/src/test/java/org/xbrlapi/data/bdbxml/framework/tests/LoadTestCase.java
+++ module-bdb-xml/src/test/java/org/xbrlapi/data/bdbxml/framework/tests/LoadTestCase.java
@@ -26,7 +26,7 @@ public class LoadTestCase extends TestCase {
     
     private final int iterations = 10000;
     private String container = "container";
-    private String location = "/home/geoff/data/bdbxml";
+    private String location = "/home/geoff/Data/bdbxml";
     private String prefix = "<a_node>";
     private String suffix = "</a_node>";
     private SimpleStore store = null;
