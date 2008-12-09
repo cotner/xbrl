9010358b0a77f3240249cb106a7dde1c7007cc11
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2008-12-09 00:50:26 +0000

    Added XDT aspect tests.

---
+++ module-xdt/src/test/java/org/xbrlapi/xdt/aspects/tests/AllTests.java
@@ -0,0 +1,16 @@
+package org.xbrlapi.xdt.aspects.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.xbrlapi.xdt.tests");
+		//$JUnit-BEGIN$
+        suite.addTestSuite(DimensionalAspectModelTestCase.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
---
+++ module-xdt/src/test/java/org/xbrlapi/xdt/aspects/tests/DimensionalAspectModelTestCase.java
@@ -0,0 +1,56 @@
+package org.xbrlapi.xdt.aspects.tests;
+
+import org.xbrlapi.FragmentList;
+import org.xbrlapi.Item;
+import org.xbrlapi.aspects.AspectModel;
+import org.xbrlapi.xdt.aspects.DimensionalAspectModel;
+import org.xbrlapi.xdt.tests.BaseTestCase;
+
+/**
+ * Tests the identification of XDT fragments.
+ * @author Geoffrey Shuetrim (geoff@galexy.net)
+ */
+public class DimensionalAspectModelTestCase extends BaseTestCase {
+
+    private final String EXPLICIT_DIMENSIONS = "test.data.local.xdt.several.explicit.dimension.values";
+    private final String TYPED_DIMENSIONS = "test.data.local.xdt.typed.dimension.values";
+    
+    
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		
+	}	
+	
+	public DimensionalAspectModelTestCase(String arg0) {
+		super(arg0);
+	}
+
+	public void testDimensionalAspectModelConstruction() {
+
+		try {
+	        loader.discover(this.getURL(this.EXPLICIT_DIMENSIONS));
+            loader.discover(this.getURL(this.TYPED_DIMENSIONS));
+	        
+	        AspectModel model = new DimensionalAspectModel();
+	        
+			FragmentList<Item> fragments = store.<Item>getFragments("NonNumericItem");
+			assertTrue(fragments.getLength() > 0);
+			for (Item fragment: fragments) {
+			    model.addFact(fragment);
+			}
+            assertEquals(9,model.getAspects().size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+	}
+	
+	
+
+
+}
