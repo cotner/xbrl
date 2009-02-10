d6ec8822277668eabc55ff93ab3c218361b56b76
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-02-10 03:34:54 +0000

    Added a new method to build an XML DOM from a URI.

--- module-utilities/src/main/java/org/xbrlapi/utilities/XMLDOMBuilder.java
+++ module-utilities/src/main/java/org/xbrlapi/utilities/XMLDOMBuilder.java
@@ -3,6 +3,8 @@ package org.xbrlapi.utilities;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URI;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -77,6 +79,21 @@ public class XMLDOMBuilder {
 		}
 	}
 
+    /**
+     * @param URI The URI of a document to parse into a DOM object.
+     * @return An XML DOM object for the given URI.
+     * @throws XBRLException if the URI corresponds to a malformed URL.
+     */
+    public Document newDocument(URI uri) throws XBRLException {
+        try {
+            return newDocument(uri.toURL().openStream());
+        } catch (MalformedURLException e) { 
+            throw new XBRLException(uri + " is a malformed URL.");
+        } catch (IOException e) { 
+            throw new XBRLException(uri + " caused an IO exception.");
+        }
+    }	
+	
 	/**
 	 * @param xml is the XML in its string representation.
 	 * @return The DOM document corresponding to the supplied XML.
