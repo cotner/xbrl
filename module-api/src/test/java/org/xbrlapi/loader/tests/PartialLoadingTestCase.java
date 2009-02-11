dddb0bdf3a1c8c27c99d51c824ad5c37b849d968
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-02-11 06:23:22 +0000

    Replaced invalid XML with a string representation of the same in the test case that uses it.

--- module-api/src/test/java/org/xbrlapi/loader/tests/PartialLoadingTestCase.java
+++ module-api/src/test/java/org/xbrlapi/loader/tests/PartialLoadingTestCase.java
@@ -15,7 +15,6 @@ public class PartialLoadingTestCase extends BaseTestCase {
 
 	private final String VALID_URI = "test.data.small.schema";
     private final String NONEXISTENT_URI = "nonexistent.uri";
-    private final String INVALIDXML_URI = "test.data.local.invalid.xml";
 
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -45,7 +44,7 @@ public class PartialLoadingTestCase extends BaseTestCase {
 
     public void testLoaderHandlingOfASAXException() throws Exception {
         try {
-            loader.stashURI(getURI(this.INVALIDXML_URI));
+            loader.discover(new URI("http://www.xbrlapi.org/invalid.xml"),"<xbrl>");
             loader.stashURI(getURI(this.VALID_URI));
             loader.discover();
         } catch (Exception e) {
