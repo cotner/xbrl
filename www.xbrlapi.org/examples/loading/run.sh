05dfeef68686a7e775da9e567ce11052443fff24
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-07-03 04:39:58 +0000

    Updated Maven usage documentation.

---
+++ www.xbrlapi.org/examples/loading/run.sh
@@ -0,0 +1,2 @@
+java -Xmx1000m -Dlog4j.configuration=test.configuration.properties -classpath=lib/* org.xbrlapi.examples.load.LoadAllSECFiles -database database -container container -cache /home/geoff/Data/taxonomy_cache -threads 4
+
