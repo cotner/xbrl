c875766d44770780b1074c54a2f84e7fe98d4e8b
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-07-03 00:51:42 +0000

    Improved handling of duplicate document utility unit tests.

--- module-bdb-xml/src/test/java/org/xbrlapi/data/bdbxml/utilities/tests/ListDuplicateDocumentsInStore.java
+++ module-bdb-xml/src/test/java/org/xbrlapi/data/bdbxml/utilities/tests/ListDuplicateDocumentsInStore.java
@@ -14,9 +14,9 @@ import org.xbrlapi.loader.Loader;
  * to reload them properly.
  * @author Geoff Shuetrim (geoff@galexy.net)
  */
-public class ReloadDuplicateDocuments extends BaseTestCase {
+public class ListDuplicateDocumentsInStore extends BaseTestCase {
     
-    public ReloadDuplicateDocuments(String arg0) {
+    public ListDuplicateDocumentsInStore(String arg0) {
         super(arg0);
     }
 
@@ -41,22 +41,11 @@ public class ReloadDuplicateDocuments extends BaseTestCase {
 
             String query = "for $document in #roots#[@parentIndex=''], $duplicate in #roots#[@parentIndex=''] where ($document/@uri=$duplicate/@uri and $document/@index!=$duplicate/@index) return string($document/@uri)";
             Set<String> uris = store.queryForStrings(query);
-            logger.info(uris.size());
+            logger.info("# duplicated documents = " + uris.size());
             for (String uri: uris) {
                 logger.info(uri + " has duplicates in the data store.");
-                query = "for $fragment in #roots#[@uri='" + uri + "']  return string($fragment/@index)";
-                Set<String> indices = store.queryForStrings(query);
-                logger.info("# fragments in document = " + indices.size());
-                for (String index: indices) {
-                    store.remove(index);
-                }
             }
             
-            logger.info("Now reloading the problem documents.");
-            for (String uri: uris) {
-                loader.discover(new URI(uri));
-            }
-           
         } catch (Exception e) {
             e.printStackTrace();
             fail("An unexpected exception was thrown.");
