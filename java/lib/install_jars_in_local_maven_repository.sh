1e5e70b1d9a46fcced024f99372f6e3339fd1aa6
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-07-04 02:22:11 +0000

    Updated the Maven local repository installation script.

--- java/lib/install_jars_in_local_maven_repository.sh
+++ java/lib/install_jars_in_local_maven_repository.sh
@@ -3,10 +3,14 @@
 mvn install:install-file -DgroupId=exist -DartifactId=exist -Dversion=1.2.6 -Dpackaging=jar -Dfile=exist-1.2.6.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=exist -DartifactId=xmldb-api -Dversion=custom -Dpackaging=jar -Dfile=xmldb.jar -DgeneratePom=true
 
+mvn install:install-file -DgroupId=org.apache.xmlrpc -DartifactId=xmlrpc-common -Dversion=3.1.1 -Dpackaging=jar -Dfile=xmlrpc-common-3.1.1.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.apache.xmlrpc -DartifactId=xmlrpc-client -Dversion=3.1.1 -Dpackaging=jar -Dfile=xmlrpc-client-3.1.1.jar -DgeneratePom=true
+
 mvn install:install-file -DgroupId=com.oracle.berkeley -DartifactId=db -Dversion=2.4.13 -Dpackaging=jar -Dfile=db.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=com.oracle.berkeley -DartifactId=dbxml -Dversion=2.4.13 -Dpackaging=jar -Dfile=dbxml.jar -DgeneratePom=true
 
 mvn install:install-file -DgroupId=net.sf.saxon -DartifactId=saxon -Dversion=9.1 -Dpackaging=jar -Dfile=saxon9.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=net.sf.saxon -DartifactId=saxon-dom -Dversion=9.1 -Dpackaging=jar -Dfile=saxon9-dom.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=net.sf.saxon -DartifactId=saxon-s9api -Dversion=9.1 -Dpackaging=jar -Dfile=saxon9-s9api.jar -DgeneratePom=true
+
 
