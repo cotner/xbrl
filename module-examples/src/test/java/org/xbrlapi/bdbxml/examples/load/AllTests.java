86fba0242e0cbc40e55f1d4783d13ea346e01181
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-07-09 04:16:17 +0000

    Fixed up package name consistency.

---
+++ module-examples/src/test/java/org/xbrlapi/bdbxml/examples/load/AllTests.java
@@ -0,0 +1,16 @@
+package org.xbrlapi.bdbxml.examples.load;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("org.xbrlapi.data.bdbxml.examples.load.tests suite");
+		//$JUnit-BEGIN$
+		suite.addTest(org.xbrlapi.data.bdbxml.examples.load.tests.AllTests.suite());
+		//$JUnit-END$
+		return suite;
+	}
+
+}
