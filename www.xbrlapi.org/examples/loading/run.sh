66cdc44b242143017a4e46f2a22d2a129607aa14
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-07-09 00:40:13 +0000

    Fixed up package name consistency.

--- www.xbrlapi.org/examples/loading/run.sh
+++ www.xbrlapi.org/examples/loading/run.sh
@@ -1,2 +1,2 @@
-java -Xmx1000m -Dlog4j.configuration=test.configuration.properties -classpath=lib/* org.xbrlapi.examples.load.LoadAllSECFiles -database database -container container -cache /home/geoff/Data/taxonomy_cache -threads 4
+java -Xmx1000m -Dlog4j.configuration=test.configuration.properties -classpath=lib/* org.xbrlapi.data.bdbxml.examples.load.LoadAllSECFiles -database database -container container -cache /home/geoff/Data/taxonomy_cache -threads 4
 
