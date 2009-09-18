92f9f988bb70a71b0a0e1b8decccc05e770d40a8
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2009-09-18 04:39:33 +0000

    Removed serializations.

--- module-api/src/main/java/org/xbrlapi/impl/FragmentImpl.java
+++ module-api/src/main/java/org/xbrlapi/impl/FragmentImpl.java
@@ -497,8 +497,6 @@ public class FragmentImpl extends XMLImpl implements Fragment {
      */
     public URI getNamespaceFromQName(String qname, Node node) throws XBRLException {
         
-        this.getStore().serialize((Element) node);
-        
         if (this.getPrefixFromQName(qname).equals("xml")) return Constants.XMLNamespace;
         
         // If we have an attribute - go straight to working with the parent element.
