a88cfe8c5b2fe7b25c064c0132ea8a3de1e62a4c
shuetrim <shuetrim@a2e666b1-2e2c-0410-97d6-c35fb8a032ad>
2010-06-22 04:23:44 +0000

    New XDT aspect handling unit tests.

---
+++ module-xdt/src/test/java/org/xbrlapi/xdt/aspects/alt/tests/AllTests.java
@@ -0,0 +1,16 @@
+package org.xbrlapi.xdt.aspects.alt.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+        TestSuite suite = new TestSuite("Test for org.xbrlapi.xdt.aspects.alt.tests");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(AspectsTestCase.class);
+        //$JUnit-END$
+        return suite;
+	}
+
+}
